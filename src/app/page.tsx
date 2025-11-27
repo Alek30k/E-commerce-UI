bd88014dec5194d62fdc1d3963368fe459583064
Alek30k <alemank23k@gmail.com>
2025-11-27 11:43:57 -0300

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-// import ProductList from "@/components/ProductList";
 import Image from "next/image";
+import ProductList from "./components/ProductList";
 
 const Homepage = async ({
   searchParams,
@@ -12,7 +12,7 @@ const Homepage = async ({
       <div className="relative aspect-[3/1] mb-12">
         <Image src="/featured.png" alt="Featured Product" fill />
       </div>
-      {/* <ProductList category={category} params="homepage"/> */}
+      <ProductList category={category} params="homepage" />
     </div>
   );
 };
