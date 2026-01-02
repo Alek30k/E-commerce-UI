d3b8fb49b37809402a3871e638a8702da2a390a3
Alek30k <alemank23k@gmail.com>
2026-01-02 16:26:13 -0300

    last commit

--- .next/static/chunks/src_613515a5._.js
+++ .next/static/chunks/src_613515a5._.js
@@ -780,7 +780,9 @@ const CartPage = ()=>{
                                 fileName: "[project]/src/app/cart/page.tsx",
                                 lineNumber: 129,
                                 columnNumber: 15
-                            }, ("TURBOPACK compile-time value", void 0))) : activeStep === 2 ? /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$15$2e$4$2e$5_react$2d$dom$40$19$2e$1$2e$0_react$40$19$2e$1$2e$0_$5f$react$40$19$2e$1$2e$0$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$ShippingForm$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
+                            }, ("TURBOPACK compile-time value", void 0))) : activeStep === 2 ? /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$15$2e$4$2e$5_react$2d$dom$40$19$2e$1$2e$0_react$40$19$2e$1$2e$0_$5f$react$40$19$2e$1$2e$0$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$ShippingForm$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
+                            setShippingForm: setShippingForm
+                        }, void 0, false, {
                             fileName: "[project]/src/app/cart/page.tsx",
                             lineNumber: 168,
                             columnNumber: 13
